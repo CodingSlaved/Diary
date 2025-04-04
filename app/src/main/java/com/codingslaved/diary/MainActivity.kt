ddd29af72c3070f34b9b9044b4be126e1d8ff186
H0kyun <h0kyungim@gmail.com>
2025-04-04 19:13:43 +0900

    feat: Enable edge-to-edge display
    - Added `enableEdgeToEdge()` in `MainActivity` to allow content to draw behind system bars.

--- app/src/main/java/com/codingslaved/diary/MainActivity.kt
+++ app/src/main/java/com/codingslaved/diary/MainActivity.kt
@@ -4,12 +4,12 @@ package com.codingslaved.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import com.codingslaved.diary.ui.Navigation
 import com.codingslaved.diary.ui.theme.DiaryTheme
-import com.codingslaved.diary.view.WritingScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
